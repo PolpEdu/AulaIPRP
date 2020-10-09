bbfd65b3dd04bf361ecbd20c9ccee6cb7b7202bb
PolpErt <63319849+PolpErt@users.noreply.github.com>
2020-10-09 10:20:13 +0100

    Organizar coisas para comecar a trabalhar

--- Exercicio 1.10.py
+++
@@ -1,9 +0,0 @@
-#Eduardo Nunes
-def ConverterTemp(Tc):
-    Tf = 9/5 *  Tc + 32
-    return Tf
-
-TC = int(input("Escolha a temperatura em Celsius: "))
-
-print("A temperatura em Fahrenheit é", ConverterTemp(TC) )
-
