59037dedc8b25b09ba64cee7be38371f230058f3
PolpErt <63319849+PolpErt@users.noreply.github.com>
2020-10-15 08:54:00 +0100

    Exercicio ainda nao acabado

--- Exercicios IPRP da aula prática/Capitulo 2/2.3.py
+++ Exercicios IPRP da aula prática/Capitulo 2/2.3.py
@@ -4,31 +4,40 @@ import random
 
 screen = Screen()
 width = 800
-height = 800
-
+height = 700
 screen.setup(width, height)
-
 vezes = random.randint(1, 500)
+pensize(2)
+
+#speed(50) é engraçado
 
 for x in range(vezes):
+    x,y = position()
+
+    #para evitar sair do ecrã
+    widthScreen = width/2
+    heightScreen = height/2
+
+    if not(-widthScreen < x < widthScreen and -heightScreen < y < heightScreen):
+        print(f"x:{x}, y:{y}")
+        print("Sai do Ecrã!")
+        penup()
+        goto(0,0)
+        pendown()
+
     graus = random.randint(0, 359) #nao pode ser 360 se nao seria = a 0
 
-    # Vou generar 3 numeros de 0 a 255 random para fazer uma cor RGB
-    Rcor = random.randint(0, 255)
-    Gcor = random.randint(0, 255)
-    Bcor = random.randint(0, 255)
+    # Vou generar 3 numeros de 0 a 1 random para fazer uma cor RGB
+    # A documentação do turtle usa numeros de 0.01 a 1
+    Rcor = random.uniform(0.01, 1)
+    Gcor = random.uniform(0.01, 1)
+    Bcor = random.uniform(0.01, 1)
 
     DirouEsq = random.randint(0, 1)
     distancia = random.randint(0, 100) #100 porque se nao sai do ecrã muito facilmente
 
-    pencolor("red")
+    pencolor(round(Rcor,2), round(Gcor,2), round(Bcor, 2))
 
-
-    x,y = position()
-
-    #para evitar sair do ecrã
-    #if -width < x < width and -height < y < height:
-    #    break
     if DirouEsq == 0:
         Dir = "Direita"
         right(graus)
@@ -37,7 +46,7 @@ for x in range(vezes):
         left(graus)
     forward(distancia)
 
-    print(f"Graus: {graus}; Distancia: {distancia}; Direção: {Dir}; A fazer Vezes: {vezes}")
+    print(f"Graus: {graus}; Distancia: {distancia}; Direção: {Dir}; Com a cor: {pencolor()}; A fazer Vezes: {vezes}")
 
 Terminator()
 done()
