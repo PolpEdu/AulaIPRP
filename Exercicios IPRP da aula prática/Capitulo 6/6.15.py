6f89174baffa9256dfcce82a8b59ae1ee50f8da6
PolpErt <63319849+PolpErt@users.noreply.github.com>
2020-11-26 13:52:58 +0000

    o resto do trabalho de hoje

--- Exercicios IPRP da aula prática/Capitulo 6/6.15.py
+++ Exercicios IPRP da aula prática/Capitulo 6/6.15.py
@@ -3,15 +3,20 @@ from random import randint
 
 
 dicionario = {
-    "Bernardo Silva":[123456789,1.90,80],
+    "Bernardo Silva":[123456789,1.90,74],
     "Fernando Pessoa":[133712320,1.60,65],
     "Verónica Matos":[190332964,1.50,50],
     "Manuel Luis Gocha":[154321982,1.80,65],
-    "Cristina Ferreira":[],
-    "Fernando Mendes":[],
-    "João Baião":[]
+    "Cristina Ferreira":[119665433,1.7,65],
+    "Fernando Mendes":[154322199,1.79,89],
+    "João Baião":[129034827,1.80,75]
 }
 
 def pessoa(dicionario):
-    return
-
+    for k,v in dicionario.items():
+        print(dicionario[k][2],(dicionario[k][1])**2)
+        massacorp= dicionario[k][2]/(dicionario[k][1])**2
+        dicionario[k].append(round(massacorp,2))
+    return dicionario
+print(pessoa(dicionario))
+#https://codeshare.io/aJEPDR
--- Exercicios IPRP da aula prática/Capitulo 6/6.18.py
+++ Exercicios IPRP da aula prática/Capitulo 6/6.18.py
@@ -1,18 +1,20 @@
 # Eduardo Nunes
 arvergeneologica = {
-    'Pais-Antunes':['PAULO'],
+    'Pais-Antunes': ['PAULO'],
     'Pais-Nunes': ['CARLOS', 'PEDRO', 'FERNANDO'],
-    'Pais-Gonçalves': ['TOMAS', 'JÚLIO', 'ERNESTO','BRUNO'],
-    'Pais-Freitas': ['TIAGO', 'CARLOTA', 'BÁRBARA', 'FRANCISCA',"CARLOTA"]
+    'Pais-Gonçalves': ['TOMAS', 'JÚLIO', 'ERNESTO', 'BRUNO'],
+    'Pais-Freitas': ['TIAGO', 'CARLOTA', 'BÁRBARA', 'FRANCISCA', "CARLOTA"]
 }
 
 
-def arvore(arvoregene,nome1,nome2):
+def arvore(arvoregene, nome1, nome2):
     for nome1, pais1 in arvoregene.items():
         for nome2, pais2 in arvoregene.items():
-            if pais1 == pais2 and nome1!=nome2:
-                print("São irmaos: ",end="")
-                print(nome1,pais1)
-                print(nome2,pais2)
-#nao funciona nao sei porque
-arvore(arvergeneologica,'CARLOS','PEDRO')
+            if pais1 == pais2 and nome1 != nome2:
+                print("São irmaos: ", end="")
+                print(nome1, pais1)
+                print(nome2, pais2)
+
+
+# nao funciona nao sei porque
+arvore(arvergeneologica, 'CARLOS', 'PEDRO')
